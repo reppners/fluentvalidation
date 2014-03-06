de29040d079281e4d823057528d28d00f569b6d6
Nathan Arnott <nathan.arnott@gmail.com>
2014-03-06 12:49:43 -0800

    Updated Test Helper to better check expected exceptions

--- FluentValidation.Tests/Helpers.cs
+++ FluentValidation.Tests/Helpers.cs
@@ -12,13 +12,21 @@ namespace FluentValidation.Tests
     {
         public static void ExpectException<TException>(Action action) where TException : Exception
         {
+            Exception ex = null;
+
             try
             {
                 action();
             }
-            catch (TException)
+            catch (TException exception)
             {
-                return;
+                if (exception.GetType() == typeof(TException)) return;
+
+                ex = exception;
+            }
+            catch(Exception exception)
+            {
+                ex = exception;
             }
 
             Assert.Fail("Expected Exception did not occur.");
