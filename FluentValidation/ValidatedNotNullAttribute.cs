1b84f7386d323e64e9b757dcec792d1ed2cf2e5c
Nathan Arnott <nathan.arnott@gmail.com>
2014-03-25 09:29:58 -0700

    Fixed problem with ValidatedNotNullAttribute

--- FluentValidation/ValidatedNotNullAttribute.cs
+++ FluentValidation/ValidatedNotNullAttribute.cs
@@ -10,7 +10,6 @@ namespace FluentValidation
     /// Indicates to Code Analysis that a method validates a particular parameter.
     /// </summary>
     [AttributeUsage(AttributeTargets.Parameter, AllowMultiple = false, Inherited = false)]
-    [Conditional("CODE_ANALYSIS")]
     public sealed class ValidatedNotNullAttribute : Attribute
     {
     }
